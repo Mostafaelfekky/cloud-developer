376b6be56a4ab6b39d86154017232476c1600ca4
Mostafaelfekky <Mostafaelfekky@gmail.com>
2021-05-29 13:56:42 +0200

    Adding the TODO

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,6 +13,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+  const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
+ 
+  app.use(expressCspHeader({
+      directives: {
+          'default-src': [SELF],
+          'img-src': ['data:', 'images.com'],
+          'worker-src': [NONE],
+          'block-all-mixed-content': true
+      }
+  }));
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -29,6 +39,33 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get("/filteredimage",async(req,res)=>{
+    console.log("in");
+    let image_url   = req.query.image_url;
+    //Validate url
+    const isValideUrl = image_url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    if(isValideUrl == null)
+      return res.status(401).send(`Inavlid url! Try again with valid url`);
+    else{
+    //Process Image
+      const filteredImage =await filterImageFromURL(image_url);
+      console.log(filteredImage);
+      if(filteredImage===undefined||filteredImage===null)
+        return res.status(401).send(`Unable to filter image`);
+      else{
+        res.on('finish', function() {
+          deleteLocalFiles([filteredImage]);  
+        });
+        return res.status(200).sendFile(''+filteredImage);
+
+      }
+      
+    }
+  })
+
+  function sendResponse(filteredImage:any){
+    return 
+  }
   //! END @TODO1
   
   // Root Endpoint
